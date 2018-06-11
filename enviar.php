98f94d9d1999a8acee274000f5be22caf4b6cc72
monzter50 <josemonzter50@gmail.com>
2018-06-10 21:05:07 -0500

    commit

--- enviar.php
+++ enviar.php
@@ -1,20 +1,10 @@
 <?php  
 
-	// // if (isset($_POST['g-recaptcha-response'])) {
-	// 	$response_recaptcha = $_POST['g-recaptcha-response'];
-	// 	$secret ="6LefTFcUAAAAAK-rZPD0FILf2Ra75Tce2_IOhVtC";
-	// 	$ip= $_SERVER['SERVER_ADDR'];
-	// 	$url="https://www.google.com/recaptcha/api/siteverify";
-	// 	$response = file_get_contents($url."?secret=".$secret."&response=".$response_recaptcha."&remoteip=".$ip);
-	// 	$jsonresponse=json_decode($response);
-	// 	echo $jsonresponse->success."<br>";
-	// 	if ($jsonresponse->success) {
-			// Llamando a los campos
+
 			$nombre = $_POST['name'];
 			$correo = $_POST['email'];
 			$mensaje = $_POST['message'];
-			// Datos para el correo
-			// $destinatario = "reservas@nomadscancun.com";
+
 			$destinatario = "apsaltidish@gmail.com";
 			$CC="josemonzter50@gmail.com";
 			$CO="jose@socialastronauts.com";
@@ -25,9 +15,5 @@
 			// Enviando Mensaje
 			mail("$destinatario,$CC,$CO", $asunto, $carta);
 			header('Location:mensaje-de-envio.html');
-	// 	}
-	// 	else{
-	// 		header('Location:../error-mensaje.html');	
-	// 	}				
-	// }
+
 ?>
